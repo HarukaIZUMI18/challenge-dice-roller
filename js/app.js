f7c31c1b6f7fac70b6dda11715765d355e21ff29
HarukaIzumi <haruka.izumi@oclock.school>
2025-11-12 17:41:13 +0100

    manque-image

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 // à toi de jouer !
+const newDiv = document.createElement("div");
+newDiv.id = "player";
+newDiv.classList.add("dice");
+player.appendChild(newDiv);
+
+
+function generateDice(){
+	const randomNumber = Math.floor(Math.random() * 6) + 1;
+    
+	document.getElementById("player").innerHTML = randomNumber;
+    document.getElementById("newDiv").src = randomNumber + ".dice"*100;
+    console.log(randomNumber);
+}
+
+
+generateDice();
